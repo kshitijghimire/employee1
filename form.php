a5a29aa62b6e9aaf77db1b455f1a707bdbdb37e0
Kshitij Ghimire <kshitijnep13@gmail.com>
2024-02-27 13:57:47 +0545

    correction

--- form.php
+++ form.php
@@ -4,21 +4,20 @@ error_reporting(0);
 // mysqli_connect_error();
 ?>
 <?php
-if(isset($_POST['searchdata']))
-{
-$search=$_POST['search'];
+if (isset($_POST['searchdata'])) {
+    $search = $_POST['search'];
 
 
-$query ="SELECT * FROM form WHERE ID='$search'";
-$data =mysqli_query($conn,$query);
+    $query = "SELECT * FROM form WHERE ID='$search'";
+    $data = mysqli_query($conn, $query);
 
-$result =mysqli_fetch_assoc($data);
+    $result = mysqli_fetch_assoc($data);
 
-// $name =$result['emp_name'];
-// echo $name;
+    // $name =$result['emp_name'];
+    // echo $name;
 
- } 
- ?>
+}
+?>
 
 
 
@@ -26,108 +25,151 @@ $result =mysqli_fetch_assoc($data);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project</title>
     <link rel="stylesheet" href="style.css" type="text/css">
 </head>
+
 <body>
     <div class="center">
         <form action="#" method="POST">
-        <h1>Employee Data Entry Automation Software</h1>
-        <div class="form">
+            <h1> Empyloee Entry Automation Software</h1>
+            <div class="form">
 
-<input type="text" name="search"  class="textfield" placeholder="Search ID" 
-value="<?php if(isset($_POST['searchdate'])){ echo $result['ID']; } ?>"
->
+                <input type="text" name="search" class="textfield" placeholder="Search ID" value="<?php if (isset($_POST['searchdata'])) {
+                                                                                                        echo $result['ID'];
+                                                                                                    } ?>">
 
 
-<input type="text" name="name" class="textfield" placeholder="Student Name" 
-value="<?php 
-if(isset($_POST['searchdate']))
-{ 
-    echo $result['emp_name']; 
-} 
-?>
-">
+                <input type="text" name="name" class="textfield" placeholder="Empyloee Name" value="<?php
+                                                                                                    if (isset($_POST['searchdata'])) {
+                                                                                                        echo $result['emp_name'];
+                                                                                                    } ?>">
 
 
 
-<select class="textfield" name="gender">
-    <option value="Not Select">Select Gender</option>
+                <select class="textfield" name="gender">
+                    <option value="Not Select">Select Gender</option>
 
-    <option value="Male" <?php if($result['emp_gender'] =='Male') {echo "selected";} ?> >Male</option>
+                    <option value="Male" <?php if ($result['emp_gender'] == 'Male') {
+                                                echo "selected";
+                                            } ?>>Male</option>
 
-    <option value="Female" <?php if($result['emp_gender'] =='Female') { echo "selected";} ?> >Female</option>
+                    <option value="Female" <?php if ($result['emp_gender'] == 'Female') {
+                                                echo "selected";
+                                            } ?>>Female</option>
 
-    <option value="Other" <?php if($result['emp_gender'] =='Other') {echo "selected";} ?> >Other</option>
+                    <option value="Other" <?php if ($result['emp_gender'] == 'Other') {
+                                                echo "selected";
+                                            } ?>>Other</option>
 
-</select>
-
-
-<input type="text" name="email" class="textfield" placeholder="Email Address"
-value="<?php if(isset($_POST['searchdate'])){ echo $result['emp_email']; } ?>" >
+                </select>
 
 
-<select class="textfield" name="department">
-    <option value="Not Select">Select Department</option>
+                <input type="text" name="email" class="textfield" placeholder="Email Address" value="<?php echo $result['emp_email']; ?>">
 
-    <option value="IT" <?php if($result['emp_department'] =='IT') {echo "selected";} ?> >IT</option>
 
-    <option value="HR" <?php if($result['emp_department'] =='HR') {echo "selected";} ?> >HR</option>
+                <select class="textfield" name="department">
+                    <option value="Not Select">Select Department</option>
 
-    <option value="Accounts" <?php if($result['emp_department'] =='Accounts') {echo "selected";} ?> >Accounts</option>
+                    <option value="IT" <?php if ($result['emp_department'] == 'IT') {
+                                            echo "selected";
+                                        } ?>>IT</option>
 
-    <option value="Sales" <?php if($result['emp_department'] =='Sales') {echo "selected";} ?> >Sales</option>
+                    <option value="HR" <?php if ($result['emp_department'] == 'HR') {
+                                            echo "selected";
+                                        } ?>>HR</option>
 
-    <option value="Marketing" <?php if($result['emp_department'] =='Marketing') {echo "selected";} ?> >Marketing</option>
+                    <option value="Accounts" <?php if ($result['emp_department'] == 'Accounts') {
+                                                    echo "selected";
+                                                } ?>>Accounts</option>
 
-    <option value="Business Development" <?php if($result['emp_department'] =='Business Development') {echo "selected";} ?> >Business Development</option>
+                    <option value="Sales" <?php if ($result['emp_department'] == 'Sales') {
+                                                echo "selected";
+                                            } ?>>Sales</option>
 
-</select>
-<textarea  name="address" placeholder="Address"><?php if(isset($_POST['searchdate'])){ echo $result['emp_address']; } ?>
+                    <option value="Marketing" <?php if ($result['emp_department'] == 'Marketing') {
+                                                    echo "selected";
+                                                } ?>>Marketing</option>
+
+                    <option value="Business Development" <?php if ($result['emp_department'] == 'Business Development') {
+                                                                echo "selected";
+                                                            } ?>>Business Development</option>
+
+                </select>
+                <textarea name="address" placeholder="Address"><?php if (isset($_POST['searchdata'])) {
+                                                                    echo $result['emp_address'];
+                                                                } ?>
 </textarea>
 
-<input type="submit" value="Search" name="searchdata" class="btn" >
+                <input type="submit" value="Search" name="searchdata" class="btn">
 
-<input type="submit"  name="save"   value="Save" class="btn" style="background-color:green;" >
+                <input type="submit" name="save" value="Save" class="btn" style="background-color:green;">
 
-<input type="submit" value="Modify" name=""   class="btn" style="background-color:orange;">
+                <input type="submit" value="Modify" name="" class="btn" style="background-color:orange;">
 
-<input type="submit" value="Delete" name=""  class="btn" style="background-color:red;">
+                <input type="submit" value="Delete" name="delete" class="btn" style="background-color:red;" onclick="return checkdelete()">
 
-<input type="submit" value="Clear"  name=""  class="btn" style="background-color:blue;">
+                <input type="submit" value="Clear" name="" class="btn" style="background-color:blue;">
 
 
 
-        </div>
+            </div>
         </form>
     </div>
-    
+
 </body>
+
 </html>
+<script>
+function checkdelete()
+{
+return  confirm('Are you sure you want to delete this rec record');
+}
+
+</script>
+
+
+
+
 <?php
-if(isset($_POST['save']))
-{
-    $name=$_POST['name'];
-    $gender=$_POST['gender'];
-    $email=$_POST['email'];
-    $department=$_POST['department'];
-    $address=$_POST['address'];
+if (isset($_POST['save'])) {
+    $name = $_POST['name'];
+    $gender = $_POST['gender'];
+    $email = $_POST['email'];
+    $department = $_POST['department'];
+    $address = $_POST['address'];
 
-    $query="INSERT INTO form(emp_name,emp_gender,emp_email,emp_department,emp_address) 
+    $query = "INSERT INTO form(emp_name,emp_gender,emp_email,emp_department,emp_address) 
     VALUES('$name','$gender','$email','$department','$address')";
-$data=mysqli_query($conn,$query);
+    $data = mysqli_query($conn, $query);
 
-if($data)
-{
-    echo "Data saved into Database";
-}
-else
-{
-    echo "Failed to save data";
-}
+    if ($data) {
+        echo " <script> alert('Data saved into Database') </script>";
+    } else {
+        echo " <script> alert('Fail to  save data  into Database') </script>";
 
+    }
+}
+?>
+
+
+
+<?php
+if (isset($_POST['delete'])) 
+{
+    $id = $_POST['search'];
+    $query = "DELETE FROM form WHERE ID = '$id' ";
+    $data = mysqli_query($conn, $query);
+    if($data) 
+    {
+        echo "Record deleted";
+    } else 
+    {
+        echo "Failed to delete";
+    }
 }
 ?>
